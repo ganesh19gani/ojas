b1cb59c9f78cd600c5b44a8785f48f43a8384d5b
GaneshAdapa <adapaganeshofficial@gmail.com>
2023-01-17 18:20:43 +0530

    code clean up

--- src/main/java/com/ojas/management/cotroller/EmployeeController.java
+++ src/main/java/com/ojas/management/cotroller/EmployeeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
     @GetMapping("/")
     public String getValues() {
-        // testing
+
         return "Hello world";
     }
 }
