41debd36b25ae4eb67743748e4e812ecabc963b8
GaneshAdapa <adapaganeshofficial@gmail.com>
2023-01-17 18:09:24 +0530

    Initial commit

---
+++ src/main/java/com/ojas/management/OjasEmployeeManagementApplication.java
@@ -0,0 +1,13 @@
+package com.ojas.management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OjasEmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OjasEmployeeManagementApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ojas/management/cotroller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.ojas.management.cotroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @GetMapping("/")
+    public String getValues() {
+        return "Hello world";
+    }
+}
